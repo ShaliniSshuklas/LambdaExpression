9930e124ffc7b31c0d95da55ab3e84a5221fe32b
Shalini Shukla <111940610+ShaliniSshuklas@users.noreply.github.com>
2023-02-07 18:42:14 +0530

    lambda expression (.net 3.5).
    It is a shorter way to represent the anonymous function .

---
+++ LambaExpression/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LambaExpression
+{
+	public class Program
+	{
+		public delegate bool IsTeenAger(Student student);
+		public static void Main(string [] args)
+        {
+			IsTeenAger isTeenAger = s => s.Age > 12 && s.Age < 25;
+			Student student = new Student() { Age = 24 };
+			Console.WriteLine(isTeenAger(student));
+		}
+	}
+	public class Student
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Age { get; set; }
+	}
+}
